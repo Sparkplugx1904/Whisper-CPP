94679efba3fc712aa5eed7144a70a395756ad2b4
Sparkplugx1904 <anandapradnyana68@gmail.com>
2026-03-12 08:34:01 +0800

    Automated Desc

--- descriptor.py
+++ descriptor.py
@@ -6,7 +6,7 @@ from google import genai
 from pydantic import BaseModel
 from typing import List
 
-# GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 def parse_model_version(name: str) -> tuple:
     """
